a8c4b9285d0e8e0ab14055c6cdb3b4ab32182017
Deeksha Muralikumar <deekshamkt@gmail.com>
2026-03-11 21:50:42 +0530

    Hospital System

--- backend/src/main/java/com/hospitalmanagement/app/controller/RootController.java
+++ backend/src/main/java/com/hospitalmanagement/app/controller/RootController.java
@@ -2,6 +2,7 @@ package com.hospitalmanagement.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @CrossOrigin(origins = "https://mahizham-hosp.vercel.app/")
